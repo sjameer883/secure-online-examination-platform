bab7c2cb10b279c7c4135737edf8d88111dcc124
sjameer883 <sjameer883@gmail.com>
2026-06-04 21:13:58 +0530

    Complete Sprint 2 Day 1 setup

---
+++ backend/secure-online-examination-platform/src/main/java/com/examportal/SecureOnlineExaminationPlatformApplication.java
@@ -0,0 +1,12 @@
+package com.examportal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SecureOnlineExaminationPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SecureOnlineExaminationPlatformApplication.class, args);
+    }
+}
